2a9dec83601768326a20eb37ce321e86c84f4925
Raul Perusquia <raul@inikoo.com>
2023-01-05 18:51:57 +0800

    permisions

---
+++ config/blueprint/permissions.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 05 Jan 2023 18:05:36 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Inikoo Ltd
+ */
+
+return [
+    'billing',
+    'users',
+    'users.edit',
+    'users.view',
+    'banners',
+    'banners.all.edit',
+    'banners.all.view',
+    'banners.all.delete',
+    'banners.own.edit',
+    'banners.own.view',
+    'banners.own.delete',
+
+];
+
---
+++ config/blueprint/roles.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Author: Raul Perusquia <raul@inikoo.com>
+ * Created: Thu, 05 Jan 2023 18:01:29 Malaysia Time, Kuala Lumpur, Malaysia
+ * Copyright (c) 2023, Inikoo Ltd
+ */
+
+return [
+    'admin'    => [
+        'billing',
+        'users'
+    ],
+    'editor'   => [
+        'banners'
+    ],
+    'designer' => [
+        'banners.own'
+    ]
+];
+
