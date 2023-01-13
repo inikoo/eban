526ddd8a3a4c65c2300fab251f0da9ab4cd31588
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-01-13 08:10:17 +0100

    EBAN-10 Modify profile page based in tailwindui

--- resources/js/Pages/Profile/EditProfile.vue
+++ resources/js/Pages/Profile/EditProfile.vue
@@ -115,7 +115,7 @@ const form = useForm({
                                 <div class="mt-6 grid grid-cols-12 gap-6">
                                     <div class="col-span-12 sm:col-span-12">
                                         <label for="first-name" class="block text-sm font-medium text-gray-700">Name</label>
-                                        <input v-model="form.name" type="text" name="first-name" id="first-name" autocomplete="given-name" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
+                                        <input v-model="form.name" type="text" name="first-name" id="first-name" autocomplete="given-name"  class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" />
                                     </div>
 
                                     <div class="col-span-12">
@@ -129,7 +129,7 @@ const form = useForm({
                             <div class="divide-y divide-gray-200">
                                 <div class="mt-4 flex justify-center py-4 px-4 sm:px-6">
                                     <button type="button" class="inline-flex justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">Cancel</button>
-                                    <button type="submit" class="ml-5 inline-flex justify-center rounded-md border border-transparent bg-sky-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">Save</button>
+                                    <button type="submit" data-test="save" class="ml-5 inline-flex justify-center rounded-md border border-transparent bg-sky-700 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2">Save</button>
                                 </div>
                             </div>
                         </form>
