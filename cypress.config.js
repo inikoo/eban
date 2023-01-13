a8000b5faac58bfa959e2972ea77402843292363
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-01-13 08:10:07 +0100

    EBAN-13 Make a test for the save button on the profile with cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '69za3z',
+  projectId: "69za3z",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "vite",
     },
   },
 });
