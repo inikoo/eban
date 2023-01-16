86939ae1c7afa2375a3fe68fba4b31be39d7feff
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-01-16 13:36:59 +0100

    EBAN-19 Give spacing, put personal/company a bit more close, adding the name of the inputs on the top

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -36,7 +36,7 @@ function showRegistration(type) {
 
 <template layout="UIMarketingLayout">
 
-    <div v-if="!showForm" class="flex min-h-full items-center justify-around py-12 px-4 sm:px-6 lg:px-8">
+    <div v-if="!showForm" class="flex min-h-full items-center justify-evenly py-12 px-4 sm:px-6 lg:px-8">
         <button class="bg-indigo-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" v-on:click="showRegistration('personal')">
             Personal
         </button>
@@ -46,7 +46,7 @@ function showRegistration(type) {
     </div>
 
     <Head title="Registration"/>
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8" v-if="showForm">
+    <div class="flex min-h-full items-center justify-center py-4 px-4 sm:px-6 lg:px-8" v-if="showForm">
         <div class="w-full max-w-md space-y-8">
             <div>
                 <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
@@ -68,44 +68,48 @@ function showRegistration(type) {
 
                 <div class="-space-y-px rounded-md shadow-sm">
                     <div>
-                        <label for="email-address" class="sr-only">Email address</label>
+                        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                        <label for="name" class="sr-only">name</label>
                         <input v-model="form.name" id="name" name="name" type="text" autocomplete="name" required=""
-                               class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                               class="relative block w-full appearance-none rounded-md border border-gray-500 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                placeholder="Name"/>
                         <InputError class="mt-2" :message="form.errors.name"/>
                     </div>
                     <div>
+                        <label for="name" class="block text-sm font-medium text-gray-700">Email address</label>
                         <label for="email-address" class="sr-only">Email address</label>
                         <input v-model="form.email" id="email-address" name="email" type="email" autocomplete="email" required=""
-                               class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                               class="relative block w-full appearance-none rounded-md border border-gray-500 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                placeholder="Email address"/>
                         <InputError class="mt-2" :message="form.errors.email"/>
                     </div>
                     <div>
+                        <label for="name" class="block text-sm font-medium text-gray-700">Password</label>
                         <label for="password" class="sr-only">Password</label>
                         <input v-model="form.password" id="password" name="password" type="password" autocomplete="new-password" required=""
-                               class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                               class="relative block w-full appearance-none rounded-md border border-gray-500 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                placeholder="Password"/>
                         <InputError class="mt-2" :message="form.errors.password"/>
                     </div>
-
                     <div>
+                        <label for="name" class="block text-sm font-medium text-gray-700">Confirm Password</label>
                         <label for="password_confirmation" class="sr-only">Password</label>
                         <input v-model="form.password_confirmation" id="password_confirmation" name="password_confirmation" type="password" autocomplete="new-password" required=""
-                               class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                               class="relative block w-full appearance-none rounded-md border border-gray-500 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                placeholder="Confirm Password"/>
                         <InputError class="mt-2" :message="form.errors.password_confirmation"/>
                     </div>
-                    <div>
+                    <div v-if="companyEnabled">
+                        <label for="name" class="block text-sm font-medium text-gray-700">Company name</label>
                         <label for="company" class="sr-only">company</label>
-                        <input  v-if="companyEnabled" v-model="form.company" id="company" name="company" type="text" autocomplete="company" required=""
-                               class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                        <input v-model="form.company" id="company" name="company" type="text" autocomplete="company" required=""
+                               class="relative block w-full appearance-none rounded-md border border-gray-500 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                placeholder="Company"/>
                         <InputError class="mt-2" :message="form.errors.company"/>
                     </div>
-                    <div>
+                    <div class="leading-9">
                         <input type="checkbox" name="terms" v-model="form.processing">
-                        <label for="terms" ><a href="#" target="_blank" > Accept terms and conditions</a></label>
+                        <label for="terms" ><a href="#" target="_blank"> Accept terms and conditions</a></label>
                     </div>
                 </div>
 
