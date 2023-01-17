97c02388fc65a0fb4fe3532f53b4dec6e806558a
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-01-17 09:43:51 +0100

    EBAN-22 Make a component with the data of home.vue (HomeSlogan.vue) / passing props and adding the component and the props to the home.vue

--- resources/js/Pages/UIMarketing/Home.vue
+++ resources/js/Pages/UIMarketing/Home.vue
@@ -2,9 +2,10 @@
 
 import {BoltIcon, DevicePhoneMobileIcon, GlobeAltIcon, ScaleIcon} from "@heroicons/vue/24/outline";
 import Features from "@/Components/UIMarketing/Sections/Features.vue";
+import HomeSlogan from "@/Components/UIMarketing/Sections/HomeSlogan.vue";
 
 const featuresData = {
-    title: "Hola",
+    title: "Hello",
     features: [
         {
             name: 'Competitive exchange rates',
@@ -33,34 +34,20 @@ const featuresData = {
     ]
 }
 
+const homeSloganData = {
+    title: 'Hello',
+    titleSpan: 'Hello2',
+    paragraph: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original.",
+    buttonLeft: "button1",
+    buttonRight: "button2",
+
+}
+
 
 </script>
 
 <template layout="UIMarketingLayout">
-
-    <div class="sm:text-center lg:text-left">
-        <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-            <span class="block xl:inline">Data to enrich your</span>
-            {{ ' ' }}
-            <span class="block text-indigo-600 xl:inline">online business</span>
-        </h1>
-        <p class="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
-            commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-        <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div class="rounded-md shadow">
-                <a href="#"
-                   class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg">Get
-                    started</a>
-            </div>
-            <div class="mt-3 sm:mt-0 sm:ml-3">
-                <a href="#"
-                   class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg">Live
-                    demo</a>
-            </div>
-        </div>
-
-    </div>
-
+    <HomeSlogan :data = "homeSloganData"></HomeSlogan>
     <features :data = "featuresData"></features>
 </template>
 
