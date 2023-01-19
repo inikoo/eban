35a0b49e0647881a11a6af7f23767156fc639c32
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-01-19 10:57:27 +0100

    EBAN-37 Pass props to the php files with the vue objects/arrays company.vue first

--- resources/js/Pages/UIMarketing/Company.vue
+++ resources/js/Pages/UIMarketing/Company.vue
@@ -1,51 +1,12 @@
 <script setup>
 
+defineProps( ['pricingData']);
 import Pricing from "@/Components/UIMarketing/Sections/Pricing.vue";
-
-const pricingData = {
-    title: 'the title',
-    paragraph: 'Paragraph',
-    paragraphTwo: 'paragraphTwo',
-    plans: {
-        'economic': {
-            name: 'Name',
-            price: 30,
-            duration: '/month',
-            button: 'Nice to meet click right now',
-            featuresEconomic: ['Pariatur quod simix11lique', 'Sapiente libero doloribus', 'Vel ipsa esse repudiandae']
-        },
-        'medium': {
-            name: 'hola',
-            price: 30,
-            duration: '/month',
-            button: 'Nice to meet click right now',
-            featuresMedium: [
-                'Quia rem est sed impedit magnam',
-                'Dolorem vero ratione voluptates',
-                'Qui sed ab doloribus voluptatem dolore',
-                'Laborum commodi molestiae id et fugiat',
-                'Nam ut ipsa nesciunt culpa modi dolor',
-            ]
-        },
-        'expensive': {
-            name: 'Name',
-            price: 30,
-            duration: '/month',
-            button: 'Nice to meet click right now',
-            featuresExpensive : [
-                'Quia rem est sed impedit magnam',
-                'Dolorem vero ratione voluptates'
-            ]
-        },
-    },
-}
 
 </script>
 
 
 <template layout="UIMarketingLayout">
-
-    <pricing :data="pricingData"></pricing>
-
+    <Pricing :data="pricingData"></Pricing>
 </template>
 
