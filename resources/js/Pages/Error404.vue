15ab9fa3d93a06938be97a6611414c47e07b30f1
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-01-19 12:26:44 +0100

    EBAN-36 Create 404 page error

---
+++ resources/js/Pages/Error404.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template layout="UIMarketingLayout">
+    <div class="bg-white">
+        <main class="mx-auto w-full max-w-7xl px-6 lg:px-8">
+            <div class="flex-shrink-0 pt-16">
+                <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+            </div>
+            <div class="mx-auto max-w-xl py-16 sm:py-24">
+                <div class="text-center">
+                    <p class="text-base font-semibold text-indigo-600">404</p>
+                    <h1 class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">This page does not exist.</h1>
+                    <p class="mt-2 text-lg text-gray-500">The page you are looking for could not be found.</p>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
