8fe90934d16314f4de0d033bac5d6dbb0de441d3
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-01-30 14:43:53 +0100

    EBAN-55 Put content in every product page (seo, adwords, emailmarketing, ads)

--- resources/js/Pages/UIMarketing/EmailMarketing.vue
+++ resources/js/Pages/UIMarketing/EmailMarketing.vue
@@ -1,9 +1,26 @@
 <script setup>
 
+import Features from "@/Components/UIMarketing/Sections/Features.vue";
+import Hero from "@/Components/UIMarketing/Sections/Hero.vue";
+import CTA from "@/Components/UIMarketing/Sections/CTA.vue";
+import Pricing from "@/Components/UIMarketing/Sections/Pricing.vue";
+import FAQ from "@/Components/UIMarketing/Sections/FAQ.vue";
+import Stats from "@/Components/UIMarketing/Sections/Stats.vue";
+import Testimonial from "@/Components/UIMarketing/Sections/Testimonial.vue";
+
+
+defineProps(['pricingData', 'ctaData', 'faqsData', 'homeSloganData', 'statsData', 'featuresData', 'testimonialData'])
+
+
+
 </script>
 
-
 <template layout="UIMarketingLayout">
- email marketing
-
+    <Hero :data="homeSloganData"></Hero>
+    <Features :data="featuresData"></Features>
+    <Stats :data="statsData"></Stats>
+    <CTA :data="ctaData"></CTA>
+    <Pricing :data="pricingData"></Pricing>
+    <Testimonial :data="testimonialData"></Testimonial>
+    <FAQ :data="faqsData"></FAQ>
 </template>
