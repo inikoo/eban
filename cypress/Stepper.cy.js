bb04409242be5256d6e327421b39f5da2e898db8
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-02-01 10:43:37 +0100

    EBAN-59 Testing cypress

--- cypress/Stepper.cy.js
+++ cypress/Stepper.cy.js
@@ -1,8 +1,10 @@
 import Stepper from '../resources/js/Components/Test.vue'
 
 describe('<Stepper />', () => {
-    it('renders', () => {
-        // see: https://on.cypress.io/mounting-vue
-        cy.mount(Stepper)
+    it('clicking + fires a change event with the incremented value', () => {
+        const onChangeSpy = cy.spy().as('onChangeSpy')
+        cy.mount(Stepper, {props: {onChange: onChangeSpy}})
+        cy.get('[data-cy=increment]').click()
+        cy.get('@onChangeSpy').should('have.been.calledWith', 1)
     })
 })
