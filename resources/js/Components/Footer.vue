a5fbd657156ed0223f22e613b6dd022073cf3737
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-02-09 10:27:48 +0100

    EBAN-76 Footer Section
    Structure for the footer section.
    
    also some style improvements on the homepage.
    
    --
    
    Solve it one visual error of the last days (when resize under 1024px appear on the right side one white bg)

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -128,36 +128,36 @@ const navigation = {
 </script>
 
 <template>
-    <footer class="bg-gray-900" aria-labelledby="footer-heading">
+    <footer class="bg-white" aria-labelledby="footer-heading">
         <h2 id="footer-heading" class="sr-only">Footer</h2>
-        <div class="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-32">
+        <div class="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-16">
             <div class="xl:grid xl:grid-cols-3 xl:gap-8">
                 <div class="grid grid-cols-2 gap-8 xl:col-span-2">
 
                     <div class="md:grid md:grid-cols-2 md:gap-8" v-for="(linkGroup) in navigationThree">
                        <div  :class="key === 'right' && 'mt-10 md:mt-0'" v-for="(linkSubGroup, key) in linkGroup">
-                            <h3 class="text-sm font-semibold leading-6 text-white">{{ linkSubGroup.label }}</h3>
+                            <h3 class="text-sm font-semibold leading-6 text-black">{{ linkSubGroup.label }}</h3>
                             <ul role="list" class="mt-6 space-y-4">
                                 <li v-for="item in linkSubGroup.items" :key="item.name">
                                     <Link :href="route(item.route)"
-                                       class="text-sm leading-6 text-gray-300 hover:text-white">{{ item.name }}</Link>
+                                       class="text-sm leading-6 text-black hover:text-black">{{ item.name }}</Link>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div class="mt-10 xl:mt-0">
-                    <h3 class="text-sm font-semibold leading-6 text-white">Subscribe to our newsletter</h3>
-                    <p class="mt-2 text-sm leading-6 text-gray-300">The latest news, articles, and resources, sent to
+                    <h3 class="text-sm font-semibold leading-6 text-black">Subscribe to our newsletter</h3>
+                    <p class="mt-2 text-sm leading-6 text-black">The latest news, articles, and resources, sent to
                         your inbox weekly.</p>
                     <form class="mt-6 sm:flex sm:max-w-md">
                         <label for="email-address" class="sr-only">Email address</label>
                         <input type="email" name="email-address" id="email-address" autocomplete="email" required=""
-                               class="w-full min-w-0 appearance-none rounded-md border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-white placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
+                               class="w-full min-w-0 appearance-none rounded-md border-white/10 bg-gray-400/10 px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] text-base leading-7 text-black placeholder-black shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
                                placeholder="Enter your email"/>
                         <div class="mt-4 rounded-md sm:mt-0 sm:ml-4 sm:flex-shrink-0">
                             <button type="submit"
-                                    class="flex w-full items-center justify-center rounded-md bg-indigo-500 py-1.5 px-3 text-base font-semibold leading-7 text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400 sm:text-sm sm:leading-6">
+                                    class="flex w-full items-center justify-center rounded-md bg-indigo-500 py-1.5 px-3 text-base font-semibold leading-7 text-black hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400 sm:text-sm sm:leading-6">
                                 Subscribe
                             </button>
                         </div>
--- resources/js/Pages/UIMarketing/HomePageTest.vue
+++ resources/js/Pages/UIMarketing/HomePageTest.vue
@@ -21,10 +21,9 @@ import {useLayoutStore} from "@/Stores/layout";
 import FAQ from "@/Components/UIMarketing/Sections/FAQ.vue";
 import YourAdvantagePenImage from "@/../../public/Images/YourAdvantagePenImage.png";
 import ContactUsGreenStuff from "@/../../public/Images/ContactUsGreenStuff.png";
-import { ref } from 'vue'
-import { Switch } from '@headlessui/vue'
-
-
+import {ref} from 'vue'
+import {Switch} from '@headlessui/vue'
+import Footer from "@/Components/Footer.vue";
 
 
 defineProps(['faqsData'])
@@ -110,7 +109,7 @@ const theme = useLayoutStore().theme
 </script>
 
 <template>
-    <div class="bg-white">
+    <div class="bg-white ">
         <header
             class="bg-[url('@/../../public/Images/HomeHeroPlantImage.jpeg')]  w-full h-[50rem] bg-auto bg-repeat-round	">
             <Popover class=" mx-auto  ">
@@ -252,26 +251,17 @@ const theme = useLayoutStore().theme
                                 </span>
                             </h2>
                             <p class="mx-auto mt-6 max-w-xl text-lg leading-6 font-light tracking-wider antialiased">
-                                Incididunt sint fugiat pariatur
-                                cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do
-                                ea.</p>
+                                Get the AW advantage of specialist retail digital marketing. More attention - our
+                                specialist team in Indonesia will spend many more hours on your account - than any
+                                European agency can provide. When you are sleeping we are working studying yesterday's
+                                results - tweeking, improving getting your best marketing ready for the next day. Awake
+                                to a real advantage everyday.
+                            </p>
                             <div class="mt-10 flex items-center justify-center gap-x-6">
                                 <a href="#"
                                    class="rounded-full text-red-60 bg-black px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm tracking-wide">Learn
                                     More</a>
                             </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"
-                                 class="absolute top-1/2 left-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
-                                 aria-hidden="true">
-                                <circle cx="512" cy="512" r="512" fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)"
-                                        fill-opacity="0.7"/>
-                                <defs>
-                                    <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
-                                        <stop offset="1" stop-color="#7775D6"/>
-                                        <stop offset="1" stop-color="#E935C1"/>
-                                    </radialGradient>
-                                </defs>
-                            </svg>
                         </div>
                     </div>
                 </div>
@@ -329,90 +319,116 @@ const theme = useLayoutStore().theme
                 <div class="md:ml-auto md:w-1/2 md:pl-10 ">
                     <div class="overflow-hidden bg-white  px-6 lg:px-8 ">
                         <div class="relative mx-auto max-w-xl">
-                            <svg class="absolute left-full translate-x-1/2 transform" width="404" height="404" fill="none" viewBox="0 0 404 404" aria-hidden="true">
+                            <svg class="absolute left-full translate-x-1/2 transform" width="404" height="404"
+                                 fill="none" viewBox="0 0 404 404" aria-hidden="true">
                                 <defs>
-                                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20"
+                                             height="20" patternUnits="userSpaceOnUse">
+                                        <rect x="0" y="0" width="4" height="4" class="text-gray-200"
+                                              fill="currentColor"/>
                                     </pattern>
                                 </defs>
-                                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+                                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"/>
                             </svg>
-                            <svg class="absolute right-full bottom-0 -translate-x-1/2 transform" width="404" height="404" fill="none" viewBox="0 0 404 404" aria-hidden="true">
+                            <svg class="absolute right-full bottom-0 -translate-x-1/2 transform" width="404"
+                                 height="404" fill="none" viewBox="0 0 404 404" aria-hidden="true">
                                 <defs>
-                                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                                    <pattern id="85737c0e-0916-41d7-917f-596dc7edfa27" x="0" y="0" width="20"
+                                             height="20" patternUnits="userSpaceOnUse">
+                                        <rect x="0" y="0" width="4" height="4" class="text-gray-200"
+                                              fill="currentColor"/>
                                     </pattern>
                                 </defs>
-                                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)" />
+                                <rect width="404" height="404" fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"/>
                             </svg>
                             <div class="text-center">
-                                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-                                <p class="mt-4 text-lg leading-6 text-gray-500">Nullam risus blandit ac aliquam justo ipsum. Quam mauris volutpat massa dictumst amet. Sapien tortor lacus arcu.</p>
+                                <h2 class="text-6xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-5xl lg:text-6xl">Contact Us</h2>
                             </div>
                             <div class="mt-12">
-                                <form action="#" method="POST" class="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                                <form action="#" method="POST"
+                                      class="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
                                     <div>
-                                        <label for="first-name" class="block text-sm font-medium text-gray-700">First name</label>
+                                        <label for="first-name" class="block text-sm font-medium text-gray-700">First
+                                            name</label>
                                         <div class="mt-1">
-                                            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+                                            <input type="text" name="first-name" id="first-name"
+                                                   autocomplete="given-name"
+                                                   class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"/>
                                         </div>
                                     </div>
                                     <div>
-                                        <label for="last-name" class="block text-sm font-medium text-gray-700">Last name</label>
+                                        <label for="last-name" class="block text-sm font-medium text-gray-700">Last
+                                            name</label>
                                         <div class="mt-1">
-                                            <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+                                            <input type="text" name="last-name" id="last-name"
+                                                   autocomplete="family-name"
+                                                   class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"/>
                                         </div>
                                     </div>
 
                                     <div class="sm:col-span-2">
                                         <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
                                         <div class="mt-1">
-                                            <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+                                            <input id="email" name="email" type="email" autocomplete="email"
+                                                   class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"/>
                                         </div>
                                     </div>
                                     <div class="sm:col-span-2">
-                                        <label for="phone-number" class="block text-sm font-medium text-gray-700">Phone Number</label>
+                                        <label for="phone-number" class="block text-sm font-medium text-gray-700">Phone
+                                            Number</label>
                                         <div class="relative mt-1 rounded-md shadow-sm">
                                             <div class="absolute inset-y-0 left-0 flex items-center">
                                                 <label for="country" class="sr-only">Country</label>
-                                                <select id="country" name="country" class="h-full rounded-md border-transparent bg-transparent py-0 pl-4 pr-8 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500">
+                                                <select id="country" name="country"
+                                                        class="h-full rounded-md border-transparent bg-transparent py-0 pl-4 pr-8 text-gray-500 focus:border-indigo-500 focus:ring-indigo-500">
                                                     <option>US</option>
                                                     <option>CA</option>
                                                     <option>EU</option>
                                                 </select>
                                             </div>
-                                            <input type="text" name="phone-number" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-gray-300 py-3 px-4 pl-20 focus:border-indigo-500 focus:ring-indigo-500" placeholder="+1 (555) 987-6543" />
+                                            <input type="text" name="phone-number" id="phone-number" autocomplete="tel"
+                                                   class="block w-full rounded-md border-gray-300 py-3 px-4 pl-20 focus:border-indigo-500 focus:ring-indigo-500"
+                                                   placeholder="+1 (555) 987-6543"/>
                                         </div>
                                     </div>
                                     <div class="sm:col-span-2">
-                                        <label for="message" class="block text-sm font-medium text-gray-700">Message</label>
+                                        <label for="message"
+                                               class="block text-sm font-medium text-gray-700">Message</label>
                                         <div class="mt-1">
-                                            <textarea id="message" name="message" rows="4" class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500" />
+                                            <textarea id="message" name="message" rows="4"
+                                                      class="block w-full rounded-md border-gray-300 py-3 px-4 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"/>
                                         </div>
                                     </div>
                                     <div class="sm:col-span-2">
                                         <div class="flex items-start">
                                             <div class="flex-shrink-0">
-                                                <Switch v-model="agreed" :class="[agreed ? 'bg-indigo-600' : 'bg-gray-200', 'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2']">
+                                                <Switch v-model="agreed"
+                                                        :class="[agreed ? 'bg-indigo-600' : 'bg-gray-200', 'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2']">
                                                     <span class="sr-only">Agree to policies</span>
-                                                    <span aria-hidden="true" :class="[agreed ? 'translate-x-5' : 'translate-x-0', 'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out']" />
+                                                    <span aria-hidden="true"
+                                                          :class="[agreed ? 'translate-x-5' : 'translate-x-0', 'inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out']"/>
                                                 </Switch>
                                             </div>
                                             <div class="ml-3">
                                                 <p class="text-base text-gray-500">
                                                     By selecting this, you agree to the
                                                     {{ ' ' }}
-                                                    <a href="#" class="font-medium text-gray-700 underline">Privacy Policy</a>
+                                                    <a href="#" class="font-medium text-gray-700 underline">Privacy
+                                                        Policy</a>
                                                     {{ ' ' }}
                                                     and
                                                     {{ ' ' }}
-                                                    <a href="#" class="font-medium text-gray-700 underline">Cookie Policy</a>.
+                                                    <a href="#" class="font-medium text-gray-700 underline">Cookie
+                                                        Policy</a>.
                                                 </p>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="sm:col-span-2">
-                                        <button type="submit" class="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Let's talk</button>
+                                        <button type="submit"
+                                                class="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                            Let's talk
+                                        </button>
                                     </div>
                                 </form>
                             </div>
@@ -426,6 +442,7 @@ const theme = useLayoutStore().theme
 
     <!-- Footer -->
 
+    <Footer></Footer>
 
 
 </template>
