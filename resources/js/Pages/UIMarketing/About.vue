08494ee2a87308c02b7aaf60bcb71863ba0fa2c8
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-02-10 12:24:08 +0100

    EBAN-79 Page about, section Who we are

--- resources/js/Pages/UIMarketing/About.vue
+++ resources/js/Pages/UIMarketing/About.vue
@@ -6,7 +6,7 @@ import YourAdvantagePenImage from "@/../../public/Images/YourAdvantagePenImage.p
 
 <template layout="UIMarketingLayout">
 
-    <div class=" bg-white  h-auto md:h-screen lg:h-screen xl:h-screen md:flex lg:flex  lg:px-3">
+    <div class=" bg-white  h-auto md:h-screen lg:h-screen xl:h-screen md:flex lg:flex">
         <div class="relative h-56 bg-indigo-600 sm:h-72  md:left-0 md:h-full md:w-1/2 lg:w-1/2 xl:w-1/2">
             <img class="h-full w-full object-cover"
                  :src="YourAdvantagePenImage"
@@ -24,7 +24,7 @@ import YourAdvantagePenImage from "@/../../public/Images/YourAdvantagePenImage.p
                 experts.
             </p>
             <br>
-            <p>
+            <p class="">
                 The AW-Advantage team bring you a host of skills to supercharge your retail business. Use our modular
                 approach - buy just the services you need - but as you add modules they will work holistically. EG. your
                 PPC guy will meet with your Social Media and make a complimentary plan - mutipling results. AW-Advantage
@@ -35,12 +35,18 @@ import YourAdvantagePenImage from "@/../../public/Images/YourAdvantagePenImage.p
                 motivated people working from our state of the art offices in Denpasar, Indonesia. Access to all types
                 of experts - including Design, PPC, Email Marketing, Social Media and more. AW-Advantage - best of both
                 worlds.
+            <br>
+                Get the AW-Advantage with your account manager based in the UK or Europe and your team of young
+                motivated people working from our state of the art offices in Denpasar, Indonesia. Access to all types
+                of experts - including Design, PPC, Email Marketing, Social Media and more. AW-Advantage - best of both
+                worlds.
 
 
             </p>
         </div>
 
     </div>
+
 
     <!--         <div class="relative mx-auto max-w-md py-12 px-6 sm:max-w-7xl sm:py-20 md:py-28 lg:px-8 lg:py-32">
             <div class="md:ml-auto md:w-1/2 md:pl-10 ">
@@ -63,6 +69,57 @@ import YourAdvantagePenImage from "@/../../public/Images/YourAdvantagePenImage.p
             </div>
         </div>
 -->
+    <section class="relative bg-gray-50 pt-10" aria-labelledby="join-heading">
+        <div class="absolute inset-x-0 hidden h-1/2 bg-blue-gray-50 lg:block" aria-hidden="true"/>
+        <div class="mx-auto max-w-7xl bg-white lg:bg-transparent lg:px-8">
+            <div class="lg:grid lg:grid-cols-12">
+                <div class="relative z-10 lg:col-span-4 lg:col-start-1 lg:row-start-1 lg:bg-transparent lg:py-16">
+                    <div class="absolute inset-x-0 h-1/2 bg-blue-gray-50 lg:hidden" aria-hidden="true"/>
+                    <div class="mx-auto max-w-md px-6 sm:max-w-3xl lg:max-w-none lg:p-0">
+                        <div class="aspect-w-10 aspect-h-6 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1">
+                            <img class="rounded-3xl object-cover object-center shadow-2xl"
+                                 src="https://images.unsplash.com/photo-1507207611509-ec012433ff52?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80"
+                                 alt=""/>
+                        </div>
+                    </div>
+                </div>
 
+                <div
+                    class="relative bg-green-600 lg:col-span-10 lg:col-start-3 lg:row-start-1 lg:grid lg:grid-cols-10 lg:items-center lg:rounded-3xl">
+                    <div class="absolute inset-0 hidden overflow-hidden rounded-3xl lg:block" aria-hidden="true">
+                        <svg
+                            class="absolute bottom-full left-full translate-y-1/3 -translate-x-2/3 transform xl:bottom-auto xl:top-0 xl:translate-y-0"
+                            width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
+                            <defs>
+                                <pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20"
+                                         patternUnits="userSpaceOnUse">
+                                    <rect x="0" y="0" width="4" height="4" class="text-white" fill="currentColor"/>
+                                </pattern>
+                            </defs>
+                            <rect width="404" height="384" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"/>
+                        </svg>
+                        <svg class="absolute top-full -translate-y-1/3 -translate-x-1/3 transform xl:-translate-y-1/2"
+                             width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
+                            <defs>
+                                <pattern id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d" x="0" y="0" width="20" height="20"
+                                         patternUnits="userSpaceOnUse">
+                                    <rect x="0" y="0" width="4" height="4" class="text-white" fill="currentColor"/>
+                                </pattern>
+                            </defs>
+                            <rect width="404" height="384" fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"/>
+                        </svg>
+                    </div>
+                    <div
+                        class="relative mx-auto max-w-md space-y-6 py-12 px-6 sm:max-w-3xl sm:py-16 lg:col-span-6 lg:col-start-4 lg:max-w-none lg:p-0 text-center">
+                        <h2 class="text-3xl font-bold tracking-tight text-black " id="join-heading">Join our team</h2>
+                        <p class="text-lg text-black ">Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate
+                            id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.</p>
+                        <a class="block w-full rounded-md border border-transparent bg-white py-3 px-5  text-base font-medium text-black shadow-md hover:bg-blue-gray-50 sm:inline-block sm:w-auto"
+                           href="#">Explore open positions</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 </template>
