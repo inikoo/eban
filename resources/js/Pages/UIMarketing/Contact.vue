1a1a93b86518d14731a988f1a15aeb6e3546fab7
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-02-13 16:19:00 +0100

    EBAN-83 Create components

--- resources/js/Pages/UIMarketing/Contact.vue
+++ resources/js/Pages/UIMarketing/Contact.vue
@@ -168,8 +168,10 @@ import MountainFieldContactPageImage from "@/../../public/Images/MountainFieldCo
             </form>
         </div>
     </div>
-    <div class="grid grid-cols-4 gap-4 h-auto text-center bg-gray-50 items-center mt-6">
-        <div class="grid object-center">
+
+    <!-- Subscribe to newsletter -->
+    <div class="grid grid-cols-4 gap-4 h-auto text-center bg-gray-50 items-center mt-6 hidden md:grid lg:grid">
+        <div class="grid object-center ">
             <img class="  object-cover"
                  :src="CavinContactPageImage"
                  alt=""/>
@@ -178,12 +180,12 @@ import MountainFieldContactPageImage from "@/../../public/Images/MountainFieldCo
         <div class="grid"></div>
         <div class="grid"></div>
         <div class="grid"></div>
-        <div class="grid col-span-2">
+        <div class=" col-span-2 grid ">
             <div class="">
                 <div class="mx-auto max-w-7xl py-12 px-6 lg:py-16 lg:px-8">
                     <h2 class="inline text-3xl font-bold tracking-tight text-gray-900 sm:block sm:text-4xl">Want product news and updates?</h2>
                     <p class="inline text-3xl font-bold tracking-tight text-indigo-600 sm:block sm:text-4xl">Sign up for our newsletter.</p>
-                    <form class="mt-8 sm:flex ml-40">
+                    <form class="mt-8 sm:flex xl:ml-12 2xl:ml-32">
                         <label for="email-address" class="sr-only">Email address</label>
                         <input id="email-address" name="email" type="email" autocomplete="email" required="" class="w-full rounded-md border-gray-300 px-5 py-3 placeholder-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs" placeholder="Enter your email" />
                         <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
@@ -205,6 +207,21 @@ import MountainFieldContactPageImage from "@/../../public/Images/MountainFieldCo
                  alt=""/></div>
         <div class="grid"></div>
         <div class="grid"></div>
+    </div>
+    <div class="grid justify-center md:hidden lg:hidden xl:hidden 2xl:hidden">
+        <div class="">
+            <div class="mx-auto max-w-7xl py-12 px-6 lg:py-16 lg:px-8">
+                <h2 class="inline text-3xl font-bold tracking-tight text-gray-900 sm:block sm:text-4xl">Want product news and updates?</h2>
+                <p class="inline text-3xl font-bold tracking-tight text-indigo-600 sm:block sm:text-4xl">Sign up for our newsletter.</p>
+                <form class="mt-8 sm:flex xl:ml-12 2xl:ml-32">
+                    <label for="email-address" class="sr-only">Email address</label>
+                    <input id="email-address" name="email" type="email" autocomplete="email" required="" class="w-full rounded-md border-gray-300 px-5 py-3 placeholder-gray-500 focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs" placeholder="Enter your email" />
+                    <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+                        <button type="submit" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Notify me</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
 </template>
