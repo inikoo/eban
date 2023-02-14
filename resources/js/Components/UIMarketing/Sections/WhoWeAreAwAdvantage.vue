d5a328f012735c19cf805011808c76ca981f0d14
Jonathan Lopez <jonathan@ancientwisdom.biz>
2023-02-14 12:30:56 +0100

    EBAN-85 Create components for the about page

---
+++ resources/js/Components/UIMarketing/Sections/WhoWeAreAwAdvantage.vue
@@ -0,0 +1,48 @@
+<script setup>
+import YourAdvantagePenImage from "@/../../public/Images/YourAdvantagePenImage.png";
+
+
+</script>
+
+<template>
+    <div class=" bg-white  h-auto  xl:h-screen md:flex lg:flex">
+        <div class="relative h-56 bg-indigo-600 sm:h-72 md:h-auto md:left-0  md:w-1/2 lg:w-1/2 xl:w-1/2">
+            <img class="h-full w-full object-cover"
+                 :src="YourAdvantagePenImage"
+                 alt=""/>
+        </div>
+        <div class="text-center mt-10 sm:w-full md:w-1/2 lg:w-1/2 xl:w-1/2 pl-20 pr-20">
+            <h2 class="font-bold text-6xl">
+                Who We Are
+            </h2>
+            <br>
+            <p>
+                Get the AW advantage of over twenty years of experience working with Pay-Per-Click, email marketing and
+                SEO. At AW we have pioneered many successful techniques over many years - now you can put them to use in
+                your business. Now utilizing the brightest and best university graduates partnered with European
+                experts.
+            </p>
+            <br>
+            <p class="">
+                The AW-Advantage team bring you a host of skills to supercharge your retail business. Use our modular
+                approach - buy just the services you need - but as you add modules they will work holistically. EG. your
+                PPC guy will meet with your Social Media and make a complimentary plan - mutipling results. AW-Advantage
+                - team work. The AW-Advatage team is rewarded and motivated by improving your business - helping you
+                grow and succeed is our reward.
+
+                Get the AW-Advantage with your account manager based in the UK or Europe and your team of young
+                motivated people working from our state of the art offices in Denpasar, Indonesia. Access to all types
+                of experts - including Design, PPC, Email Marketing, Social Media and more. AW-Advantage - best of both
+                worlds.
+                <br>
+                Get the AW-Advantage with your account manager based in the UK or Europe and your team of young
+                motivated people working from our state of the art offices in Denpasar, Indonesia. Access to all types
+                of experts - including Design, PPC, Email Marketing, Social Media and more. AW-Advantage - best of both
+                worlds.
+
+
+            </p>
+        </div>
+
+    </div>
+</template>
